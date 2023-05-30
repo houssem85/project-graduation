ab0195ac377b0878d1baab701e98f9ca33605e94
houssemeddine daoud <h.eddine-daoued@oxton-digital.com>
2023-05-30 12:10:12 +0100

    project

--- presentation/src/main/java/com/programasoft/presentation/reservations/ReservationsScreen.kt
+++ presentation/src/main/java/com/programasoft/presentation/reservations/ReservationsScreen.kt
@@ -1,10 +1,39 @@
 package com.programasoft.presentation.reservations
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 
 @Composable
 fun ReservationsRoute() {
-
+    var state by remember { mutableStateOf(0) }
+    val titles = listOf("OLD", "NEW")
+    Column {
+        TabRow(selectedTabIndex = state) {
+            titles.forEachIndexed { index, title ->
+                Tab(
+                    selected = state == index,
+                    onClick = { state = index },
+                    text = { Text(text = title, maxLines = 2, overflow = TextOverflow.Ellipsis) }
+                )
+            }
+        }
+        Text(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            text = "Text tab ${state + 1} selected",
+            style = MaterialTheme.typography.bodyLarge
+        )
+    }
 }
 
 @Composable
